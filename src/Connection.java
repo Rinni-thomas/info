2bededc138655b4edf447fca23b6785aa89c8025
Rinni-thomas <49278372+Rinni-thomas@users.noreply.github.com>
2019-04-17 16:32:53 +0530

    first commit

---
+++ src/Connection.java
@@ -0,0 +1,24 @@
+*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.PreparedStatement;
+*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author robinthomas
+ */
+class Connection {
+
+    PreparedStatement peparedStatement(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
